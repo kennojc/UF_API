00a1aa482dd5154699f954c7642090b3b28e9574
Kenneth Johnson C <kennojc@gmail.com>
2021-02-22 22:32:15 -0300

    Updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :values
+  resources :users  
+  
+  get 'by_date/:date', to: 'values#by_date'
+  get ':username', to: 'users#by_name'
+
 end
+
+
